aa75631b7923585568d0da291a5bb365c0017c7d
Raheel0954 <Raheel@haspatal.com>
2025-07-26 19:16:03 +0500

    index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+$(document).ready(function () {
+  $('.ticker-wrapper').on('mouseenter', function () {
+    $('.ticker-content').css('animation-play-state', 'paused');
+  });
 
+  $('.ticker-wrapper').on('mouseleave', function () {
+    $('.ticker-content').css('animation-play-state', 'running');
+  });
+});
 
 
